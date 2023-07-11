375ec0a613afcbf524f6a51c25bbad0625f3f6e7
shaikh sufyan <shaikhsufyan1811@gmail.com>
2023-07-11 10:01:03 +0530

    Javascript Challenges Question

---
+++ First Challenge/index.js
@@ -0,0 +1,8 @@
+function myFun(){
+    let container = document.querySelector(".container");
+    container.style.backgroundColor="yellow";
+    container.style.width="600px";
+    container.style.height="600px";
+
+
+}
